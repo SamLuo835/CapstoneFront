0aaf027193af5f0b57ec694e69a34626a2515558
Jianlin Luo <sam@Jianlins-MacBook-Pro.local>
2019-05-05 14:07:36 -0400

    bike status selection

--- src/app/_components/home/bike-inventory/bike-inventory.component.ts
+++ src/app/_components/home/bike-inventory/bike-inventory.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CoreService } from '../../../_service/core.service';
 import { DataShareService } from 'src/app/_service/data-share.service';
+
 @Component({
   selector: 'app-bike-inventory',
   templateUrl: './bike-inventory.component.html',
   styleUrls: ['./bike-inventory.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class BikeInventoryComponent implements OnInit {
 
+
+export class BikeInventoryComponent implements OnInit {
   constructor(private _core :CoreService,private _dataShare:DataShareService) { }
 
   bikes:Array<any>; 
   showSpinner : boolean = true
+
+
 
   ngOnInit() {
     //check data share bikelist first
@@ -26,12 +31,30 @@ export class BikeInventoryComponent implements OnInit {
 
 
 
+  
+
   getBikeList(){
     this._core.getBikeList().subscribe(res=>{
       this.showSpinner = false;
       this.bikes = JSON.parse(res) ;
-      //calling this will trigger the subscribe event that listening on bike list in other component
-      this._dataShare.changeBikeList(this.bikes)})
+    //TODO this need to be removed, as the back end need to return bike status as string 
+      for(var i in this.bikes){
+        this.bikes[i].available = this.bikes[i].available.toString()
+      }
+    //calling this will trigger the subscribe event that listening on bike list in other component
+      this._dataShare.changeBikeList(this.bikes)
+      console.log(this.bikes)
+    })
+
+     
+    }
+
+
+  changeStatus(i){
+      console.log(this.bikes[i]);
+      //call back end service to change status of bike
+
+      //this._dataShare.changeBikeList(this.bikes)
     }
   }
 
